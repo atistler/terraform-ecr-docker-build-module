2a84fc628a76102bd50e9db2c76df079d71c7d5e
Adam Tistler <adam.tistler@rackspace.com>
2020-08-03 15:29:41 -0400

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -7,6 +7,7 @@ set -e
 build_folder=$1
 aws_ecr_repository_url_with_tag=$2
 aws_region=$3
+aws_profile=$4
 
 # Allow overriding the aws region from system
 if [ "$aws_region" != "" ]; then
@@ -15,11 +16,17 @@ else
   aws_extra_flags=""
 fi
 
+if [ "$aws_profile" != "" ]; then
+  aws_extra_flags="--profile $aws_profile"
+else
+  aws_extra_flags=""
+fi
+
 # Check that aws is installed
 which aws > /dev/null || { echo 'ERROR: aws-cli is not installed' ; exit 1; }
 
 # Connect into aws
-$(aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin $aws_ecr_repository_url_with_tag) || { echo 'ERROR: aws ecr login failed' ; exit 1; }
+$(aws ecr get-login-password $aws_extra_flags | docker login --username AWS --password-stdin $aws_ecr_repository_url_with_tag) || { echo 'ERROR: aws ecr login failed' ; exit 1; }
 
 # Check that docker is installed and running
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
