db9a392ef3b1cbc6ecc2fddc2e7d3c2e841ac271
Adam Tistler <adam.tistler@rackspace.com>
2020-08-04 07:32:16 -0400

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -7,17 +7,12 @@ set -e
 build_folder=$1
 aws_ecr_repository_url_with_tag=$2
 aws_region=$3
-aws_profile=$4
 
 # Allow overriding the aws region from system
 if [ "$aws_region" != "" ]; then
   aws_extra_flags="--region $aws_region"
 else
   aws_extra_flags=""
-fi
-
-if [ "$aws_profile" != "" ]; then
-  aws_extra_flags="$aws_extra_flags --profile $aws_profile"
 fi
 
 # Check that aws is installed
